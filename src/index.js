5627cec888cc88ccb729aa461e1541a5065824b0
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-05-28 08:03:09 +0100

    API free trial ended - update which data is retrieved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './styles/style.css'
-import getWeatherData from './js/getWeatherData';
-
 
 // Font awesome:
 import "@fortawesome/fontawesome-free/js/fontawesome";
--- src/js/getWeatherData.js
+++ src/js/getWeatherData.js
@@ -1,11 +1,14 @@
-function getHour(date) {
-  const hour = date.getHours();
-  return hour;
-}
+/* Lines in this file that are commented out are removed from code
+because free trial of API has ended and data is not available anymore
+*/
+// function getHour(date) {
+//   const hour = date.getHours();
+//   return hour;
+// }
 
 export default async function getWeatherData(city) {
   const now = new Date();
-  const currentHour = getHour(now);
+  // const currentHour = getHour(now);
 
   const apiKey = "dbd4f8b9dc4f40dc87284401231305";
   const urlCurrent = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
@@ -21,13 +24,13 @@ export default async function getWeatherData(city) {
     const dataMarine = await responseMarine.json();
     const { wind_kph, wind_dir, temp_c, precip_mm, pressure_mb, cloud, uv } =
       dataCurrent.current;
-    const { swell_ht_mt, swell_dir } =
-      dataMarine.forecast.forecastday[0].hour[currentHour];
+    // const { swell_ht_mt, swell_dir } =
+    //   dataMarine.forecast.forecastday[0].hour[currentHour];
     const data = {
       "Wind Speed": wind_kph,
       "Wind Direction": wind_dir,
-      "Wave Height": swell_ht_mt,
-      "Wave Direction": swell_dir,
+      // "Wave Height": swell_ht_mt,
+      // "Wave Direction": swell_dir,
       "Atmospheric Pressure": pressure_mb,
       Temperature: temp_c,
       Precipitation: precip_mm,
